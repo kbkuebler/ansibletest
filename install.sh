f67a0f7312e34f54b1c918ab5c7c384044172b2a
Brian Kuebler <bkuebler@fedora.localdomain>
2020-04-21 10:05:42 -0400

    Added hostname check.

--- install.sh
+++ install.sh
@@ -3,17 +3,25 @@
 #Brian Kuebler 4/17/20
 
 #Install necessary packages, only python2 installed
-echo "####  Installing Python3 and Ansible  ####"
-yum -y install epel-release
-yum -y install python3
-yum -y install python3-pip
 
-yum -y install centos-release-ansible-29
-yum -y install ansible
-yum -y install vim
-sleep 1
+APACKG=( epel-release python3 python3-pip centos-release-ansible-29 ansible vim )
+HNAME=$(hostname)
+
+
+echo "####  Installing Python3 and Ansible  ####"
+
+for pkg in "${APACKG[@]}";do
+    if yum -q list installed "$pkg" > /dev/null 2&>1; then
+        echo -e "$pkg is already installed"
+    else
+        yum install "$pkg" -y && echo "Successfully installed $pkg"
+    fi
+done
+
+
 
 #Install SDK 
+
 echo "####  Installing the Pure Storage SDK  ####"
 pip3 install purestorage
 
@@ -34,7 +42,7 @@ set softtabstop=4       " number of spaces in tab when editing
 set tabstop=4           " number of visual spaces per TAB
 EOF
 
-#We need to change the hostname of this host. Note that it's "linux" on teh F
+#We need to change the hostname of this host. Note that it's "linux" on the FA
 # and it's "Linux" on the host. 
 
 echo "#### Changing hostname ####"
@@ -42,6 +50,11 @@ echo "#### Changing hostname ####"
 echo "linux" > /etc/hostname
 systemctl restart systemd-hostnamed
 
-#make sure that we change the interpreter to python3!
-#this is from the testing branch
-#added while testing is checked out
+
+for lname in 'linux';do
+    if [ "$HNAME" = "$lname" ]; then
+        echo "Hostname is linux, matches FlashArray."
+    else
+        echo "Hostname still needs to be changed!"
+    fi
+done
