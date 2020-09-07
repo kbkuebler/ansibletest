a8cbafa1ea7650dceb193c59c652c8134ae7b22f
kbkuebler <bkuebler@gmail.com>
2020-09-07 15:22:50 -0400

    added new setup paramaters and vimrc as separate file.

---
+++ setup.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# This is only to be used as a prep for the Pure Test Drive Environment
+# It will install the most optimal Python3 version, as well as set up Ansible.
+# Brian Kuebler 9/7/20
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+#Call for any external install files
+kubesprayinstall = "./installKubernetes.sh"     #get the most recent \
+                                                #kubesprayinstall
+ansibleinstall = "./installAnsible.sh"
+# Install necessary packages. Currently, only python2 installed.
+
+
+echo "#############################################################"
+
+function installPackages() {
+  #install all required Linux packages
+  APACKG=( epel-release
+           python3 python3-pip
+           centos-release-ansible-29
+           ansible
+           vim
+           python2-jmespath )
+
+  echo "####  Installing Python3 and Ansible  ####"
+
+  for pkg in "${APACKG[@]}";do
+      if yum -q list installed "$pkg" > /dev/null 2>&1; then
+          echo -e "$pkg is already installed"
+      else
+          yum install "$pkg" -y && echo "Successfully installed $pkg"
+      fi
+  done
+
+#You don't need to use these, but they can help with less typing.
+
+  echo "" >> ~/.bashrc
+  echo "alias ap='ansible-playbook'" >> ~/.bashrc
+  echo "alias P='cd ~/ansibletest/Playbooks'" >> ~/.bashrc
+}
+
+function installAnsible() {
+  #statements
+  if [[ -f $ansibleinstall ]]; then
+    echo "will run file $ansibleinstall"
+    $ansibleinstall
+  else
+    echo "Please check to make sure that $kubesprayinstall exists"
+  fi
+}
+
+function installKubernetes() {
+  #statements
+  if [[ -f $kubesprayinstall ]]; then
+    echo "running $kubesprayinstall"
+    $kubesprayinstall
+  else
+    echo "Please check to make sure that $kubesprayinstall exists"
+  fi
+}
+
+#Let's run everything
+installPackages
+installAnsible
+installKubernetes
+
+sleep 3
+
+echo "Installation complete. Run 'source .bashrc'\
+ in order to use new aliases."
---
+++ vimrc
@@ -0,0 +1,9 @@
+cat << 'EOF' >> ~/.vimrc
+set incsearch           " search as characters are entered
+set hlsearch            " highlight matches
+colo torte              " set colorscheme
+syntax on               " syntax highlighing on
+set expandtab           " tabs are spaces
+set softtabstop=4       " number of spaces in tab when editing
+set tabstop=4           " number of visual spaces per TAB
+EOF
